1e0e12bc78f1bc76465405148b7e90333eca9d7f
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2025-06-19 08:48:42 -0400

    added file for steps

--- RPi-3B/pi3-pre-ros.bash
+++ RPi-3B/pi3-pre-ros.bash
@@ -51,8 +51,21 @@ if ["$SETUP_STATE" = "swapfile" && $SWAP_FILE=false]
     swapon --show
 
     echo "SETUP_STATE=ros-install" > "$CONFIG_FILE"
-
 fi
 
-if []
+if ["$SETUP_STATE" = "ros-install"]
+    apt install software-properties-common
+    sudo add-apt-repository universe
+
+    sudo apt update && sudo apt install curl -y
+    export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+    curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+    sudo apt install /tmp/ros2-apt-source.deb
+
+    sudo apt update && sudo apt install ros-dev-tools
+    sudo apt update && sudo apt upgrade
+    sudo apt install ros-jazzy-ros-base
+
+    source /opt/ros/jazzy/setup.bash
+
 fi
