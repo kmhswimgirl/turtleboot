b9c17738bb040a2f3b7b9838627699553234ab49
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2025-06-26 09:58:40 -0400

    aplha version of turtleboot done

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # **RBE3002-tb3-setup**
-bash scripts to auto install ROS (Jazzy) on a Turtlebot3 at various points of the setup process
+Bash scripts to auto install ROS (Jazzy) on a Turtlebot3 at various points of the setup process.
 
-### Which bash script should I use?
+## Which bash script should I use?
 
-First navigate to the folder that corresponds to the model of RPi on the turtlebot3
+That depends on what status your microSD card is in!
 
-Raspberry Pi 3 B+
+`turtleboot.bash` info:
+--
+### Features:
+- Regenerates ssh keys
+- Changes the hostname
+- Changes `ROS_DOMAIN_ID`
 
-Raspberry Pi 4
+### Requirements:
+- Ubuntu Server 24.04 is installed
+- ROS Jazzy Jalisco is already
+- Wi-fi connection not required
+
+### Example:
+I want to change my hostname to "hello-there", my `ROS_DOMAIN_ID` to 22 and I do not want to rebuild the turtlebot3_ws.
+
+First make the script executable with:
+</br>
+`chmod +x scriptname.bash`
+
+Run the script:
+</br>
+`sudo ./turtleboot.bash "hello-there" "22" false`
---
+++ turtleboot.bash
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Reconfiguring the network settings / regenerating SSH Key only after a microSD clone
+# Status --> UNTESTED
+
+# exit on errors
+set -e
+
+# variables from cl args
+TURTLEBOT_NAME="turtlebot3-clone"
+ROS_ID="30"
+REBUILD="false"
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --name|-n)
+            TURTLEBOT_NAME="$2"
+            shift 2
+            ;;
+        --ros-id|-id)
+            ROS_ID="$2"
+            shift 2
+            ;;
+        --rebuild)
+            REBUILD="true"
+            shift
+            ;;
+        --no-rebuild)
+            REBUILD="false"
+            shift
+            ;;
+        --help|-h)
+            echo "Usage: $0 [--name|-n NAME] [--ros-id|-id ID] [--rebuild|--no-rebuild]"
+            exit 0
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+cat << "EOF"
+ _____           _   _      ____              _   
+|_   _|   _ _ __| |_| | ___| __ )  ___   ___ | |_ 
+  | || | | | '__| __| |/ _ \  _ \ / _ \ / _ \| __|
+  | || |_| | |  | |_| |  __/ |_) | (_) | (_) | |_ 
+  |_| \__,_|_|   \__|_|\___|____/ \___/ \___/ \__|
+                   _____     ____
+                 /      \  |  o | 
+                |        |/ ___\| 
+                |_________/     
+                |_|_| |_|_|
+EOF
+
+echo "Welcome to TurtleBoot!"
+echo "developed by @kmhswimgirl"
+
+# confirm args
+echo "TurtleBot Name: $TURTLEBOT_NAME"
+echo "Rebuild flag: $REBUILD"
+echo "ROS Domain ID: $ROS_ID"
+
+# delete old ssh key
+rm -v /etc/ssh/ssh_host_*
+
+# regen SSH Key
+dpkg-reconfigure openssh-server
+
+# restart ssh server
+systemctl restart ssh
+
+echo "SSH keys regenerated!"
+
+# cloud-init parameter change
+sed -i "15s/.*/preserve_hostname: true/" /etc/cloud/cloud.cfg
+
+# writing in the new hostname
+hostnamectl set-hostname $TURTLEBOT_NAME
+sed -i "2s/.*/127.0.0.1 $TURTLEBOT_NAME/" /etc/hosts
+echo "hostname set to $TURTLEBOT_NAME !"
+
+# cd into turtlebot3_ws
+cd ~/turtlebot3_ws/
+
+# rebuild tb3 packages?
+if [ -d ~/turtlebot3_ws ]; then
+    cd ~/turtlebot3_ws
+    if [ "$REBUILD" = true ]; then
+        colcon build --symlink-install 
+    fi
+fi
+
+echo "Reconfiguration Finished!"
+reboot
+exit 0
